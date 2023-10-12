55ebb9cc3c5a26b09142b6be6193424791ed25a5
ehgenong <pyzlypz@gmail.com>
2023-10-12 12:36:19 +0800

    Update fujixweekly.js

--- Scripts/fujixweekly.js
+++ Scripts/fujixweekly.js
@@ -1,11 +1,20 @@
-const myStatus = "HTTP/1.1 200 OK";
-const myHeaders = {"Content-Type": "text/html; charset=utf-8","Access-Control-Allow-Origin": "*"};
-const myData = "<script>var xhr=new XMLHttpRequest();xhr.open('GET','https://fxwapp.s3.amazonaws.com/FXW+App+Recipes.xlsx',true);xhr.onreadystatechange=function(){if(xhr.readyState===4&&xhr.status===200){var blob=xhr.response;var a=document.createElement('a');var url=window.URL.createObjectURL(blob);a.href=url;a.download='FXW+App+Recipes.xlsx';a.click();window.URL.revokeObjectURL(url)}};xhr.send();</script>";
-
-const myResponse = {
-    status: myStatus,
-    headers: myHeaders, 
-    body: myData 
+const url = "https://fxwapp.s3.amazonaws.com/FXW+App+Recipes.xlsx";
+const myRequest = {
+    url: url
 };
 
-$done(myResponse);
+$task.fetch(myRequest).then(response => {
+    const myStatus = "HTTP/1.1 200 OK";
+    const myHeaders = {
+        "Content-Type": "text/html; charset=utf-8"
+    };
+    const myData = "<script>document.addEventListener('DOMContentLoaded',function(){const byteArray=" + response.bodyBytes + ";const blob=new Blob([byteArray],{type:'application/octet-stream'});const url=URL.createObjectURL(blob);const a=document.createElement('a');a.href=url;a.download='FXW+App+Recipes.xlsx';a.style.display='none';document.body.appendChild(a);a.click();URL.revokeObjectURL(url);document.body.removeChild(a)});</script>";
+
+    const myResponse = {
+        status: myStatus,
+        headers: myHeaders,
+        body: myData
+    };
+
+    $done(myResponse);
+});
