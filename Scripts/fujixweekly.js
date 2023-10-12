4a84580cff7a2931c1df1ca3441f27125dc5c51d
ehgenong <pyzlypz@gmail.com>
2023-10-12 11:14:48 +0800

    Update fujixweekly.js

--- Scripts/fujixweekly.js
+++ Scripts/fujixweekly.js
@@ -1,16 +1,3 @@
-//https://fxwapp.s3.amazonaws.com/FXW+App+Recipes.xlsx
-const url = "https://github.com/emtry/QX/raw/main/Scripts/FXW+App+Recipes.xlsx";
-const myRequest = {
-    url: url
-};
-
-$task.fetch(myRequest).then(response => {
-    $done({bodyBytes: response.bodyBytes});
-}, reason => {
-    $done();
-});
-
-
 var modifiedHeaders = $response.headers;
 
 modifiedHeaders['Content-Type'] = 'text/html; charset=utf-8';
