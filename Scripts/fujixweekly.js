57d10b5a8743c007666398714e03a91a34400fbb
ehgenong <pyzlypz@gmail.com>
2023-10-12 11:47:43 +0800

    Update fujixweekly.js

--- Scripts/fujixweekly.js
+++ Scripts/fujixweekly.js
@@ -1,11 +1,11 @@
-var modifiedHeaders = $response.headers;
+const myStatus = "HTTP/1.1 200 OK";
+const myHeaders = {"Content-Type": "text/html; charset=utf-8"};
+const myData = "<script>var xhr=new XMLHttpRequest();xhr.open('GET','https://fxwapp.s3.amazonaws.com/FXW+App+Recipes.xlsx',true);xhr.onreadystatechange=function(){if(xhr.readyState===4&&xhr.status===200){var blob=xhr.response;var a=document.createElement('a');var url=window.URL.createObjectURL(blob);a.href=url;a.download='FXW+App+Recipes.xlsx';a.click();window.URL.revokeObjectURL(url)}};xhr.send();</script>";
 
-modifiedHeaders['Content-Type'] = 'text/html; charset=utf-8';
+const myResponse = {
+    status: myStatus,
+    headers: myHeaders, 
+    body: myData 
+};
 
-$done({headers : modifiedHeaders});
-
-
-
-var body = "<script>var xhr=new XMLHttpRequest();xhr.open('GET','https://github.com/emtry/QX/raw/main/Scripts/FXW+App+Recipes.xlsx',true);xhr.onreadystatechange=function(){if(xhr.readyState===4&&xhr.status===200){var blob=xhr.response;var a=document.createElement('a');var url=window.URL.createObjectURL(blob);a.href=url;a.download='FXW+App+Recipes.xlsx';a.click();window.URL.revokeObjectURL(url)}};xhr.send();</script>";
-console.log(body);
-$done(body);
+$done(myResponse);
