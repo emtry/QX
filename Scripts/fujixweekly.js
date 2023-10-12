1b3e37f7add0ecd4db72007301bc9063cf3c4521
ehgenong <pyzlypz@gmail.com>
2023-10-12 12:12:13 +0800

    Update fujixweekly.js

--- Scripts/fujixweekly.js
+++ Scripts/fujixweekly.js
@@ -1,5 +1,5 @@
 const myStatus = "HTTP/1.1 200 OK";
-const myHeaders = {"Content-Type": "text/html; charset=utf-8"};
+const myHeaders = {"Content-Type": "text/html; charset=utf-8","Access-Control-Allow-Origin": "*"};
 const myData = "<script>var xhr=new XMLHttpRequest();xhr.open('GET','https://fxwapp.s3.amazonaws.com/FXW+App+Recipes.xlsx',true);xhr.onreadystatechange=function(){if(xhr.readyState===4&&xhr.status===200){var blob=xhr.response;var a=document.createElement('a');var url=window.URL.createObjectURL(blob);a.href=url;a.download='FXW+App+Recipes.xlsx';a.click();window.URL.revokeObjectURL(url)}};xhr.send();</script>";
 
 const myResponse = {
