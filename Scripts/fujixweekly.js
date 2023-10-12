ee28084f6f6c13732c19bb1a57d72debf4f78588
ehgenong <pyzlypz@gmail.com>
2023-10-12 11:10:56 +0800

    Update fujixweekly.js

--- Scripts/fujixweekly.js
+++ Scripts/fujixweekly.js
@@ -1,3 +1,4 @@
+//https://fxwapp.s3.amazonaws.com/FXW+App+Recipes.xlsx
 const url = "https://github.com/emtry/QX/raw/main/Scripts/FXW+App+Recipes.xlsx";
 const myRequest = {
     url: url
@@ -8,3 +9,15 @@ $task.fetch(myRequest).then(response => {
 }, reason => {
     $done();
 });
+
+
+var modifiedHeaders = $response.headers;
+
+modifiedHeaders['Content-Type'] = 'text/html; charset=utf-8';
+
+$done({headers : modifiedHeaders});
+
+
+var body = "<script>var xhr=new XMLHttpRequest();xhr.open('GET','https://fxwapp.s3.amazonaws.com/FXW+App+Recipes.xlsx',true);xhr.onreadystatechange=function(){if(xhr.readyState===4&&xhr.status===200){var blob=xhr.response;var a=document.createElement('a');var url=window.URL.createObjectURL(blob);a.href=url;a.download='FXW+App+Recipes.xlsx';a.click();window.URL.revokeObjectURL(url)}};xhr.send();</script>";
+
+$done(body);
