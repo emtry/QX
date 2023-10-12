f87af159803d28abd3087c4b65e9de148a519351
ehgenong <pyzlypz@gmail.com>
2023-10-12 14:57:26 +0800

    Update fujixweekly.js

--- Scripts/fujixweekly.js
+++ Scripts/fujixweekly.js
@@ -1,23 +1,10 @@
-const url = "https://fxwapp.s3.amazonaws.com/FXW+App+Recipes.xlsx";
+const url = "https://github.com/emtry/QX/raw/main/Scripts/FXW+App+Recipes.xlsx";
 const myRequest = {
     url: url
 };
 
 $task.fetch(myRequest).then(response => {
-    var decoder = new TextDecoder('utf8');
-    var b64encoded = btoa(decoder.decode(response.bodyBytes));
-    console.log(b64encoded);
-    const myStatus = "HTTP/1.1 200 OK";
-    const myHeaders = {
-        "Content-Type": "text/html; charset=utf-8"
-    };
-    const myData = "<script>document.addEventListener('DOMContentLoaded',function(){const byteArray=" + response.bodyBytes + ";const blob=new Blob([byteArray],{type:'application/octet-stream'});const url=URL.createObjectURL(blob);const a=document.createElement('a');a.href=url;a.download='FXW+App+Recipes.xlsx';a.style.display='none';document.body.appendChild(a);a.click();URL.revokeObjectURL(url);document.body.removeChild(a)});</script>";
-
-    const myResponse = {
-        status: myStatus,
-        headers: myHeaders,
-        body: myData
-    };
-
-    //$done(myResponse);
+    $done({bodyBytes: response.bodyBytes});
+}, reason => {
+    $done();
 });
