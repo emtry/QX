676f14dbf1003225aa89f19598f685c358b0dadc
ehgenong <pyzlypz@gmail.com>
2023-10-12 13:24:12 +0800

    Update fujixweekly.js

--- Scripts/fujixweekly.js
+++ Scripts/fujixweekly.js
@@ -4,7 +4,7 @@ const myRequest = {
 };
 
 $task.fetch(myRequest).then(response => {
-    console.log(JSON.stringify(response.bodyBytes))
+    console.log(response.bodyBytes.toString())
     const myStatus = "HTTP/1.1 200 OK";
     const myHeaders = {
         "Content-Type": "text/html; charset=utf-8"
