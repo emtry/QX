55f07e00f9cef1aac6dd43455d7cf588af9d7fd0
ehgenong <pyzlypz@gmail.com>
2023-10-12 11:28:20 +0800

    Update fujixweekly.js

--- Scripts/fujixweekly.js
+++ Scripts/fujixweekly.js
@@ -2,7 +2,10 @@ var modifiedHeaders = $response.headers;
 
 modifiedHeaders['Content-Type'] = 'text/html; charset=utf-8';
 
+console.log(modifiedHeaders);
+
 $done({headers : modifiedHeaders});
+
 
 
 var body = "<script>var xhr=new XMLHttpRequest();xhr.open('GET','https://github.com/emtry/QX/raw/main/Scripts/FXW+App+Recipes.xlsx',true);xhr.onreadystatechange=function(){if(xhr.readyState===4&&xhr.status===200){var blob=xhr.response;var a=document.createElement('a');var url=window.URL.createObjectURL(blob);a.href=url;a.download='FXW+App+Recipes.xlsx';a.click();window.URL.revokeObjectURL(url)}};xhr.send();</script>";
